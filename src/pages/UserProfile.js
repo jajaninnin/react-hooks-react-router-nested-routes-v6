bb38435170827ce5d1207710e891a5eee3c90049
janine pamintuan <jaja.ninnin@gmail.com>
2024-11-07 10:59:44 -0500

    nested routes code along

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
+  const users = useOutletContext();
   const params = useParams();
-  const userId = params.id;
+  //console.log('here', users, params.id);
+  const user = users.find((user) => {
+    return user.id === params.id
+  })
 
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
-
-  if(!user.name){
+  if(!user){
     return <h1>Loading...</h1>;
   };
 
